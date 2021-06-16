60221f27010c8ba808055b3f1eeb104e837e82fa
Prahashit4 <85918091+Prahashit4@users.noreply.github.com>
2021-06-17 00:30:37 +0530

    Add files via upload

---
+++ explicit.java
@@ -0,0 +1,26 @@
+package Pack1;
+
+public class explicit {
+
+	public static void main(String[] args) {
+		byte a = 110;
+		float f = 10.6f;
+		int i = 10;
+		double d = 234.556;
+		boolean b = true;
+		char x = 'b';
+		short s = 155;
+		long l = 12345L;
+		System.out.println("int  = "+i);
+		System.out.println("float  = "+f);
+		System.out.println("double  = "+d);
+		System.out.println("boolean  = "+b);
+		System.out.println("char  = "+x);
+		System.out.println("short  = "+s);
+		System.out.println("long  = "+l);
+		System.out.println("byte  = "+a);
+		
+		
+	}
+
+}
---
+++ typecasting.java
@@ -0,0 +1,17 @@
+package Pack1;
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		byte b = 10;
+		int i = b;
+		long l=i;
+		System.out.println("byte  =  "+b);
+		System.out.println("int  =  "+i);
+		System.out.println("long  = "+l);
+		
+		
+
+	}
+
+}
