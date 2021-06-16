f38c3df60f3cdcf2ef5131e629ffedb2c71065ee
Prahashit4 <85918091+Prahashit4@users.noreply.github.com>
2021-06-16 23:59:03 +0530

    Add files via upload

---
+++ address.java
@@ -0,0 +1,20 @@
+package Pack1;
+
+public class address {
+
+	public static void main(String[] args) {
+		String name = "prahashit";
+		String Dno = "9-5-20/4";
+		String city = "rajahmundry";
+		String state = "andra predesh";
+		System.out.println("address  :");
+		System.out.println("---------------");
+		System.out.println("Name = "+name);
+		System.out.println("DNO = "+Dno);
+		System.out.println("City = "+city);
+		System.out.println("State  ="+state);
+		
+
+	}
+
+}
