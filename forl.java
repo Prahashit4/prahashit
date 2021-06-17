4b3b500c35d9448ca7343d3337c5fdaa6a357f7a
Prahashit4 <85918091+Prahashit4@users.noreply.github.com>
2021-06-17 22:28:36 +0530

    Add files via upload

---
+++ forl.java
@@ -0,0 +1,13 @@
+package Pack1;
+
+public class forl {
+
+	public static void main(String[] args) {
+		for(int i =10;i>=0;i--)
+			System.out.println(i);
+		
+		
+
+	}
+
+}
---
+++ if2.java
@@ -0,0 +1,23 @@
+package Pack1;
+
+import java.util.*;
+
+public class if2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number :");
+		
+		int i = sc.nextInt();
+		
+		if(i%2 != 0 )
+			System.out.println("odd number");
+		else
+			System.out.println("even number");
+		
+				
+			
+
+	}
+
+}
