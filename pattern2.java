159f41d6a6a1e3ae7063c85b1854501256725b3a
Prahashit4 <85918091+Prahashit4@users.noreply.github.com>
2021-06-19 10:24:03 +0530

    Add files via upload

--- pattern2.java
+++ pattern2.java
@@ -1,16 +1,17 @@
-package Pack1;
+package chinna;
 
 public class pattern2 {
 
 	public static void main(String[] args) {
 		for(int i = 1 ; i <= 5 ; i++ )
-		{ 
+		{
 			for(int j = 1 ; j <= 5 ; j++ )
-				{
-				System.out.print(j);
-				}
+			{
+				System.out.print(i);
+			}
 			System.out.println();
 		}
+		
 
 	}
 
