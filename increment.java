986b7dece80ec43cc8cd9462e4744c528336109f
Prahashit4 <85918091+Prahashit4@users.noreply.github.com>
2021-07-03 23:59:22 +0530

    Add files via upload

---
+++ increment.java
@@ -0,0 +1,17 @@
+package chinna;
+import java.util.*;
+public class increment {
+
+	public static void main(String[] args) {
+		try (Scanner scan = new Scanner(System.in)) {
+			System.out.println("enter number :");
+			int a=scan.nextInt();
+			int c=--a;
+			System.out.println("result = "+c);
+		}
+	    
+		
+		
+	}
+
+}
